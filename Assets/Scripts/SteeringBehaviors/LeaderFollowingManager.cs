375c39abe16c66371a22091123335232256acbc6
FloRul <rumiel.florian@gmail.com>
2019-06-24 03:08:10 -0400

    FirstShot for leader following

---
+++ Assets/Scripts/SteeringBehaviors/LeaderFollowingManager.cs
@@ -0,0 +1,26 @@
+﻿using RIT.AI.Flocking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderFollowingManager : MonoBehaviour
+{
+    [SerializeField] FlockingAgent Leader;
+    [SerializeField] FlockingAgent[] Followers;
+
+    void Start()
+    {
+        Leader.AddFlocking(new WanderStrategy(Leader, 1, new WanderParams(Vector3.zero, 10, .05f, 1, 1)));
+        for (int i = 0; i < Followers.Length; i++)
+        {
+            var follower = Followers[i];
+            follower.AddFlocking(new LeaderFollowing(follower, 1, Leader, 1));
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(Leader.Position,Leader.Position - Leader.Velocity.normalized * 1);
+    }
+}
